5803ed290009cff37501e257080dda088e9c0ad2
Joseph J. Abuzaid <jabuzaid@gmail.com>
2019-05-04 13:50:11 -0700

    Add files via upload

--- sandbox/app.py
+++ sandbox/app.py
@@ -1,3 +1,6 @@
+### Joe J. Abuzaid ###
+### UC Berkeley Data Analytics Bootcamp ###
+### 2018-2019 ###
 import os
 
 import pandas as pd
@@ -98,4 +101,7 @@ def samples(sample):
 
 
 if __name__ == "__main__":
-    app.run()
+    # Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+    # app.run()
